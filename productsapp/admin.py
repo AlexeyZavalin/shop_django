027e3aa71ac0f7c9fd9187e79fc1282333af5384
Alexey Zavalin <lehazavalin95@gmail.com>
2021-01-11 18:23:15 +1000

    changed ignore added models

--- productsapp/admin.py
+++ productsapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from productsapp.models import Category, Product, ProductImage
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductImage)
+class ImageAdmin(admin.ModelAdmin):
+    pass
